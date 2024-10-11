430f08a78bffdea5ed74f5fbbee8816b94d3e05c
astn <github@msg.austinh.io>
2024-10-11 16:29:39 -0700

    Init first item

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ buttons.forEach((btn) => {
     paragraph.dataset.active = (!active).toString();
   });
 });
+
+const firstParagraph = buttons[0].parentElement.querySelector(
+  '.card-paragraph-wrapper'
+);
+
+const firstImageSet = buttons[0].parentElement.querySelectorAll('img');
+
+firstParagraph.dataset.active = 'true';
+firstImageSet.forEach((img) => {
+  let imgActive = img.dataset.active === 'true';
+  img.dataset.active = (!imgActive).toString();
+});
