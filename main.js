c8448712bbfe7f13035f0a49eec2176426cd556f
astn <github@msg.austinh.io>
2024-10-11 16:30:57 -0700

    Simplified init by removing js and just setting html data

--- main.js
+++ main.js
@@ -16,19 +16,3 @@ buttons.forEach((btn) => {
     paragraph.dataset.active = (!active).toString();
   });
 });
-
-function initFirstItem() {
-  const firstParagraph = buttons[0].parentElement.querySelector(
-    '.card-paragraph-wrapper'
-  );
-
-  const firstImageSet = buttons[0].parentElement.querySelectorAll('img');
-
-  firstParagraph.dataset.active = 'true';
-  firstImageSet.forEach((img) => {
-    let imgActive = img.dataset.active === 'true';
-    img.dataset.active = (!imgActive).toString();
-  });
-}
-
-initFirstItem();
