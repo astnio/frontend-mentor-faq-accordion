472c1fab2ca275ffbb8dcf724cfc492553eb8d6e
astn <github@msg.austinh.io>
2024-10-11 16:30:00 -0700

    put init into function

--- main.js
+++ main.js
@@ -17,14 +17,18 @@ buttons.forEach((btn) => {
   });
 });
 
-const firstParagraph = buttons[0].parentElement.querySelector(
-  '.card-paragraph-wrapper'
-);
+function initFirstItem() {
+  const firstParagraph = buttons[0].parentElement.querySelector(
+    '.card-paragraph-wrapper'
+  );
 
-const firstImageSet = buttons[0].parentElement.querySelectorAll('img');
+  const firstImageSet = buttons[0].parentElement.querySelectorAll('img');
 
-firstParagraph.dataset.active = 'true';
-firstImageSet.forEach((img) => {
-  let imgActive = img.dataset.active === 'true';
-  img.dataset.active = (!imgActive).toString();
-});
+  firstParagraph.dataset.active = 'true';
+  firstImageSet.forEach((img) => {
+    let imgActive = img.dataset.active === 'true';
+    img.dataset.active = (!imgActive).toString();
+  });
+}
+
+initFirstItem();
