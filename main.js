d1f41df38338c78fa9e8a93d0a8cf086bc0b1998
astn <github@msg.austinh.io>
2024-10-11 16:25:20 -0700

    Buttons now change icon

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ buttons.forEach((btn) => {
     const paragraph = e.target.parentElement.querySelector(
       '.card-paragraph-wrapper'
     );
+    const images = e.target.parentElement.querySelectorAll('img');
+
+    images.forEach((img) => {
+      let imgActive = img.dataset.active === 'true';
+      img.dataset.active = (!imgActive).toString();
+    });
 
     let active = paragraph.dataset.active === 'true';
     paragraph.dataset.active = (!active).toString();
